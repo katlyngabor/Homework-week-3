dcd0f826eb5b58d22c54afbb083ebcf5a6f338bf
Saun Whittenburg <saunwhittenburg@Sauns-MacBook-Pro.local>
2014-06-19 17:05:02 -0400

    started added second set of data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,22 @@
-$('.search-bar').append(prompt)
+
+var cID= clientID || '';
+var cSECRET= clientSecret||'';
+
+var template = _.template( $('#sidebar_template').html() );
+ 
+var info=$.getJSON('https://api.github.com/users/katlyngabor').done( function (data) {
+
+  $('#sidebar').html( template(data));
+
+});
+
+var templateTwo= _.template( $('#main_body_template').html() );
+
+
+$.getJSON('https://api.github.com/users/katlyngabor/repos').done(function (data) {
+	
+	$('#main-body').html( templateTwo(data[0]));
+	
+});
+
+// console.log(info_two);
---
+++ app/scripts/secret.js
@@ -0,0 +1,3 @@
+
+var clientID='7fba8993ef3393061fb9';
+var clientSecret='8bf5415b5588d67c8d34e7af170268252048821a';
