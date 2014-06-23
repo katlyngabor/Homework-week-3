69ec6693be2111a9b6828bd56ac168c8cf7e1acf
Saun Whittenburg <saunwhittenburg@Sauns-MacBook-Pro.local>
2014-06-23 08:40:54 -0400

    Updated

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,25 @@
-console.log('The Iron Yard Rocks');
+var cID= clientID || '';
+var cSECRET= clientSecret||'';
+
+var template = _.template( $('#sidebar_template').html() );
+ 
+var info=$.getJSON('https://api.github.com/users/katlyngabor').done( function (data) {
+
+  $('#sidebar').html( template(data));
+
+});
+
+
+var templateTwo= _.template( $('#main_body_template').html());
+ 
+
+var trial= $.getJSON('https://api.github.com/users/katlyngabor/repos').done(function (data) {
+_.each(data,function(info){
+$('#main-body').append(templateTwo(info));
+
+});
+
+});
+
+console.log(trial);
+
